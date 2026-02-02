ecce07c2193cbca1aec7086bca99df9250ffa2eb
Cole Nelson <ctnelson2@wisc.edu>
2026-02-02 14:36:26 -0600

    wdb3

---
+++ starter/index.js
@@ -0,0 +1,23 @@
+// This is where your JS goes!
+
+const things = ["dogs", 1.2, 0, false, {name: "Alice"}, -7];
+
+// fetch('https://cs571api.cs.wisc.edu/rest/s26/ice/chili', {
+//     headers: {
+//         "X-CS571-ID": CS571.getBadgerId()
+//     }
+// })
+// .then(res => {
+//     console.log(res.status);
+//     if(res.status === 200) {
+//         return res.json();
+//     } else {
+//         throw new Error();
+//     }
+// })
+// .then(data => {
+//     console.log(data);
+// })
+// .catch(err => {
+//     alert("Uh oh! Something went wrong.")
+// })
